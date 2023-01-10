fbd66f018038c816bc9b166c8105faef43081c92
Emilio Higuita <emionepunch@gmail.com>
2023-01-10 15:34:25 -0500

    se agrego la pagina pages donde se creo el item del menu y se le dio navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PorfolioComponent } from './pages/porfolio/porfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemsComponent } from './pages/items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: PorfolioComponent},
+  { path: "about", component: AboutComponent},
+  { path: "Items", component: ItemsComponent},
+  { path: "**", pathMatch: "full", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
